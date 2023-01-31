94540452892e067be059111cba187883b3ab148e
Nélio Dias <jrneliodias@gmail.com>
2023-01-31 20:23:03 -0300

    Atualizando o app Clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Cliente, Carro
+import re
 # Create your views here.
 def clientes(request):
-    return render(request,'clientes.html')
+    if request.method == "GET":
+        return render(request,'clientes.html')
+    
+    elif request.method == "POST":
+        # Receber os dados do formulário
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+        
+        cliente = Cliente.objects.filter(cpf=cpf)
+        
+        if cliente.exists():
+            return render(request,'clientes.html',
+                                {'nome':nome,
+                                 'sobrenome':sobrenome,
+                                 'email': email})
+        
+        email_validation = r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
+        
+        if not re.fullmatch(re.compile(email_validation), email):
+            return render(request,'clientes.html',
+                                {'nome':nome,
+                                 'sobrenome':sobrenome,
+                                 'cpf': cpf})
+        
+        # Criar o cliente na tabela
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+        cliente.save()
+        # Inserir na tabela de carros
+        for carro, placa,ano in zip(carros,placas,anos):
+            carro = Carro(carro = carro, placa =placa,ano=ano,cliente =cliente)
+            carro.save()
+            
+        return HttpResponse('Teste')
--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -1,11 +1,18 @@
-function add_carro(){
+function add_carro() {
+  container = document.getElementById("form-carro");
 
-    console.log('Teste')
-    {% comment %} container = document.getElementById("form-carro")
+  html =   `<br />
+            <div class="row">
+            <div class="col-md">
+                <input type="text" placeholder="Carro" class="form-control" name="carro" />
+            </div>
+            <div class="col-md">
+                <input type="text" placeholder="Placa" class="form-control" name="placa" />
+            </div>
+            <div class="col-md">
+                <input type="number" placeholder="Ano" class="form-control" name="ano" />
+            </div>
+            </div>`;
 
-    /html = "<br><div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name=""></div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control'> </div></div>"
-
-    container.innerHTML += html {% endcomment %}
-
-
+  container.innerHTML += html;
 }
