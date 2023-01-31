f7894fd70a336c9103d551e0d91221244fa57239
Nélio Dias <jrneliodias@gmail.com>
2023-01-31 00:24:44 -0300

    Começar a criar o app Clientes

---
+++ templates/static/clientes/js/clientes.js
@@ -0,0 +1,11 @@
+function add_carro(){
+
+    console.log('Teste')
+    {% comment %} container = document.getElementById("form-carro")
+
+    /html = "<br><div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name=""></div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control'> </div></div>"
+
+    container.innerHTML += html {% endcomment %}
+
+
+}
