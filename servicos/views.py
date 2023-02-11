2ab7ea9c7e61324e12034b61b1b630766a66ab26
Nélio Dias <jrneliodias@gmail.com>
2023-02-11 18:23:35 -0300

    Atualizado novo_servico

--- servicos/views.py
+++ servicos/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from .forms import FormServico
+from django.http import HttpResponse
+from django.contrib.messages import add_message, constants
 
 # Create your views here.
 def novo_servico(request):
-    form = FormServico()
-    return render(request, 'novo_servico.html',{'form':form})
+    if request.method == "GET":
+        form = FormServico()
+        return render(request, 'novo_servico.html',{'form':form})
+    elif request.method == "POST":
+        form = FormServico(request.POST)
+        if form.is_valid():
+            form.save()
+            add_message(request,constants.SUCCESS, "Cliente atualizado com sucesso!")
+            form2 = FormServico()
+            return render(request, 'novo_servico.html',{'form':form2})
+        
+        else:
+            add_message(request,constants.ERROR, "Campo preenchido incorretamente!")
+            return render(request, 'novo_servico.html',{'form':form})
 
+    
